84a6ac460969d7eff1d9a86ee6f4ec6fac7008f6
Gabriel Ovalles <ovalles.gabriel@gmail.com>
2020-08-28 09:29:20 -0500

    Update Login Page Elements and user, password

--- cypress/integration/PageObject/PimLogin.js
+++ cypress/integration/PageObject/PimLogin.js
@@ -3,9 +3,9 @@ import React from "react";
 
 class PimLogin {
   visit() {
-    cy.visit("https://pimqa.realpageaim.net/AIMUI/portfolio");
+    cy.visit("https://aimunified-wmu.realpageaim.net/AIMUI/portfolio");
     //cy.visit("https://pim-wmu.asseteye.net/AIMUI/portfolio")
-    cy.get("[class='login-form-container']").should('be.visible')
+    //cy.get("[class='login-form-container']").should("be.visible");
   }
 
   creDentials() {
@@ -16,19 +16,19 @@ class PimLogin {
         password: testDataRow.password,
       };
       context(`Generating a test for ${data.username}`, () => {
-        const email = cy.get("#username");
+        const email = cy.get("#Username");
         email.clear();
         email.type(data.username);
-        const pass = cy.get("#password");
+        const pass = cy.get("#Password");
         pass.clear();
         pass.type(`${data.password}`);
-        pass.wait(1000)
-        });
+        pass.wait(1000);
+      });
     });
   }
 
   submit() {
-    const button = cy.get("[type='submit']");
+    const button = cy.get("button[value='login']");
     button.click();
   }
 }
